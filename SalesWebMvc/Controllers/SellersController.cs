b0847ea837c0f8ed9c562f5825e0578ab702504c
Junior Cesar <juniorcesarr2009@gmail.com>
2019-03-30 14:47:04 -0300

    Delet seller

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -38,5 +38,25 @@ namespace SalesWebMvc.Controllers
             _selerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
+        public IActionResult Delete(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var obj = _selerService.FindById(id.Value);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _selerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
